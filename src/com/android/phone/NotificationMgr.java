omnirom/android_packages_services_Telephony

Telephony: Break qti-telephony-framework dependency

src/com/android/phone/NotificationMgr.java
@@ -113,9 +113,6 @@ public class NotificationMgr {
     // used to track whether the message waiting indicator is visible, per subscription id.
     private ArrayMap<Integer, Boolean> mMwiVisible = new ArrayMap<Integer, Boolean>();
 
-    private IExtTelephony mExtTelephony = IExtTelephony.Stub.
-            asInterface(ServiceManager.getService("extphone"));
-
     /**
      * Private constructor (this is a singleton).
      * @see #init(PhoneGlobals)
@@ -682,14 +679,20 @@ public class NotificationMgr {
                         serviceState + " new network " + networkSelection);
 
                 try {
-                    //get current provision state of the SIM.
-                    provisionStatus = mExtTelephony.getCurrentUiccCardProvisioningStatus(slotId);
+                    IExtTelephony mExtTelephony = IExtTelephony.Stub.
+                            asInterface(ServiceManager.getService("extphone"));
+                    Log.d("maxwen", "mExtTelephony=" + mExtTelephony);
+                    if (mExtTelephony != null) {
+                        //get current provision state of the SIM.
+                        provisionStatus = mExtTelephony.getCurrentUiccCardProvisioningStatus(slotId);
+                    } else {
+                        provisionStatus = PROVISIONED;
+                    }
                 } catch (RemoteException ex) {
                     provisionStatus = INVALID_STATE;
                     if (DBG) log("Failed to get status for slotId: "+ slotId +" Exception: " + ex);
-                } catch (NullPointerException ex) {
-                    provisionStatus = INVALID_STATE;
-                    if (DBG) log("Failed to get status for slotId: "+ slotId +" Exception: " + ex);
+                } catch (NoClassDefFoundError ex) {
+                    provisionStatus = PROVISIONED;
                 }
 
                 if (serviceState == ServiceState.STATE_OUT_OF_SERVICE

syberia-project/platform_packages_services_Telephony

Change-Id: I3c09525c3d1fe2c58a7ef2a1521adcd82d7dd6f8
Signed-off-by: DennySPB <dennyspb@gmail.com>

src/com/android/phone/NotificationMgr.java
@@ -829,16 +829,18 @@ public class NotificationMgr {
         int provisionStatus = INVALID_STATE;
         IExtTelephony mExtTelephony = IExtTelephony.Stub
                 .asInterface(ServiceManager.getService("extphone"));
-        int slotId = SubscriptionController.getInstance().getSlotIndex(subId);
-        try {
-            //get current provision state of the SIM.
-            provisionStatus = mExtTelephony.getCurrentUiccCardProvisioningStatus(slotId);
-        } catch (RemoteException ex) {
-            provisionStatus = INVALID_STATE;
-            if (DBG) log("Failed to get status for slotId: "+ slotId +" Exception: " + ex);
-        } catch (NullPointerException ex) {
-            provisionStatus = INVALID_STATE;
-            if (DBG) log("Failed to get status for slotId: "+ slotId +" Exception: " + ex);
+        if (mExtTelephony != null) {
+            int slotId = SubscriptionController.getInstance().getSlotIndex(subId);
+            try {
+                //get current provision state of the SIM.
+                provisionStatus = mExtTelephony.getCurrentUiccCardProvisioningStatus(slotId);
+            } catch (RemoteException ex) {
+                provisionStatus = INVALID_STATE;
+                if (DBG) log("Failed to get status for slotId: "+ slotId +" Exception: " + ex);
+            } catch (NullPointerException ex) {
+                provisionStatus = INVALID_STATE;
+                if (DBG) log("Failed to get status for slotId: "+ slotId +" Exception: " + ex);
+            }
         }
         return provisionStatus == PROVISIONED;
    }