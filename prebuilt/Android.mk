dfa0fbab958d550a245a61ae6a9673d8dcc4e985
DennySPb <dennyspb@gmail.com>
2020-07-24 14:06:42 +0300

    Conditionally ship qti Sim Settings
    Modded: added dark theme support
    
    Change-Id: Ib6464b61cbcbdd80a9d179ce5ac923410a828a46
    Signed-off-by: DennySPb <dennyspb@gmail.com>

---
+++ prebuilt/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(PRODUCT_WANTS_QTI_SIM_SETTINGS),true)
+include $(CLEAR_VARS)
+LOCAL_MODULE := QtiSimSettings
+LOCAL_SRC_FILES := QtiSimSettings.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_PRODUCT_MODULE := true
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+include $(BUILD_PREBUILT)
+endif
